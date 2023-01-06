d85a50c0cdea61f85cf449e5d037389237dd4e1c
Douglas Williams <douglaswilliams298@gmail.com>
2023-01-06 01:20:09 -0300

    Fix: Se aplicado esse commit ajustei uns arquivos na master

---
+++ src/endpoints/getAllTurmas.ts
@@ -0,0 +1,4 @@
+import { Turmas } from '../types'
+import { connection } from '../database/connections'
+import { Request, Response } from 'express'
+
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import app from './app';
+//import { getAllTurmas } from './endpoints/getAllTurmas';
 // import getAllCharacters from './endpoints/getAllCharacters';
 // import putCreateCharacter from './endpoints/putCreateCharacter';
 // import deleteCharacter from './endpoints/deleteCharacter';
@@ -8,3 +9,6 @@ import app from './app';
 // app.put("/characters", putCreateCharacter)
 
 // app.delete("/characters/:id", deleteCharacter)
+
+
+//app.get("/docentes", getAllTurmas)
