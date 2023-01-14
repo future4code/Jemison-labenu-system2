d705974f2c36157a14d2114754e1f75c24c8d7a2
Douglas Williams <douglaswilliams298@gmail.com>
2023-01-14 18:34:35 -0300

    Se aplicado esse commit, criei a classe DocentesEspecialidade

--- src/class/DocentesEspecialidades.ts
+++ src/class/DocentesEspecialidades.ts
@@ -1,5 +1,23 @@
-export type DocentesEspecialidades_data = {
-    id?: number,
-    docente_id: number,
-    especialidade_id: number
+export class DocentesEspecialidades {
+    private id?: number | undefined;
+    private docente_id: number;
+    private especialidade_id: number;
+
+    constructor (id: number, docente_id: number, especialidade_id: number){
+        this.id = id;
+        this.docente_id = docente_id;
+        this.especialidade_id = especialidade_id;
+    }
+
+    public getId(): number | undefined {
+        return this.id;
+    }
+
+    public getDocente_id(): number {
+        return this.docente_id;
+    }
+
+    public getEspecialidade_id(): number {
+        return this.especialidade_id;
+    }
 }
